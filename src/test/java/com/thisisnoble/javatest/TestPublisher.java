392eac7951b7605b8317d774ab1c779014f5e29e
Chi-Cheong Weng <chicheongweng@gmail.com>
2014-04-24 19:08:04 +0800

    make testPublisher() thread safe

--- src/test/java/com/thisisnoble/javatest/TestPublisher.java
+++ src/test/java/com/thisisnoble/javatest/TestPublisher.java
@@ -1,17 +1,31 @@
 package com.thisisnoble.javatest;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.thisisnoble.javatest.impl.CompositeEvent;
+
 public class TestPublisher implements Publisher {
 
-    private Event lastEvent;
-
+    private Object listLock = new Object();
+    
+    final List<CompositeEvent> list = Collections.synchronizedList(new ArrayList<CompositeEvent>());
+    
     @Override
     public void publish(Event event) {
-        this.lastEvent = event;
+        list.add((CompositeEvent)event);
     }
 
     public Event getLastEvent() {
-        Event result = lastEvent;
-        lastEvent = null;
-        return result;
+    	// We need to synchronize here to avoid the situation
+    	// where there is only one element in the list and
+    	// multiple threads trying to remove that same element
+    	synchronized(listLock) {
+    		CompositeEvent ce = null;
+    		if (list.size()>0)
+    			ce = list.remove(0);
+    		return ce;
+    	}
     }
 }
